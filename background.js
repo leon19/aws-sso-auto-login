92801226da42ba904f47a89c39bed980c86b6828
Lorens León <lorensleon@gmail.com>
2024-10-17 19:47:23 +0200

    feat: faster interaction with the AWS login page

--- background.js
+++ background.js
@@ -9,31 +9,27 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.url?.startsWith('chrome://')) return undefined;
 
     if (changeInfo.status === 'complete') {
-        setTimeout(() => {
-            chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                function: checkAndLogin,
-                args: [tabId]
-            });
-        }, 1500); // Delay of 1.5 seconds
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            function: checkAndLogin,
+            args: [tabId]
+        });
     }
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'closeTab') {
-        setTimeout(() => {
-            chrome.tabs.remove(message.tabId);
-        }, 1500); // Delay of 1.5 seconds
+        chrome.tabs.remove(message.tabId);
     }
 });
 
-function checkAndLogin(tabId) {
+async function checkAndLogin(tabId) {
     try {
         const currentURL = window.location.href;
         // Check if the current tab URL contains 'device.sso.*.awamazon.com'
         const isSSOPage = currentURL.includes('device.sso') && currentURL.includes('amazonaws.com');
         if (isSSOPage) {
-            const loginButton = document.getElementById('cli_verification_btn');
+            const loginButton =  await waitForElement(() => document.getElementById('cli_verification_btn'));
             if (loginButton) {
                 // Click the 'cli_verification_btn'
                 loginButton.click();
@@ -43,7 +39,7 @@ function checkAndLogin(tabId) {
         // Check if the current tab URL contains 'awsapps.com' and 'start'
         const isUserConsentPage = currentURL.includes('awsapps.com') && currentURL.includes('start');
         if (isUserConsentPage) {
-            const loginButtonAfterRedirection = document.getElementById('cli_login_button');
+            const loginButtonAfterRedirection = await waitForElement(() => document.getElementById('cli_login_button'));
             // check if the 'cli_login_button' exists
             if (loginButtonAfterRedirection) {
                 // Click the 'cli_login_button'
@@ -56,23 +52,42 @@ function checkAndLogin(tabId) {
         // this function is used if the button id changes and will find the english text in the button to allow access
         const isNewUserConsentPage = currentURL.includes('awsapps.com') && currentURL.includes('start');
         if (isNewUserConsentPage) {
-            // Get all buttons on the page
-            const buttons = document.getElementsByTagName('button');
-            for (let i = 0; i < buttons.length; i++) {
-                // If the button's text content is 'Allow access', click it
+            const allowButton = await waitForElement(() => {
+                const button = document.querySelector('button[data-testid="allow-access-button"]');
 
-                const isAllowButton = buttons[i].textContent.toLowerCase().includes('allow access') || buttons[i].getAttribute('data-testid') === 'allow-access-button';
-
-                if (isAllowButton) {
-                    buttons[i].click();
-                    console.log('Clicked on Allow access button');
-                    chrome.runtime.sendMessage({ action: 'closeTab', tabId: tabId });
-                    break;
+                if (button?.textContent.toLowerCase().includes('allow access')) {
+                    return button;
                 }
+            });
+
+            if (allowButton) {
+                allowButton.click();
+                console.log('Clicked on Allow access button');
+
+                await waitForElement(() => document.querySelector('.awsui-context-alert')?.textContent.toLocaleLowerCase().startsWith('request approved'));
+
+                chrome.runtime.sendMessage({ action: 'closeTab', tabId: tabId });
             }
         }
 
     } catch (error) {
         console.error('An error occurred:', error);
+    }
+
+    function wait(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function waitForElement(fn, timeout = 1500) {
+        const startTime = Date.now();
+
+        do {
+            const result = fn();
+            if (result) {
+                return result;
+            }
+
+            await wait(100);
+        } while (Date.now() - startTime < timeout);
     }
 }
